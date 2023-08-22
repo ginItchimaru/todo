098890ed0508dbd12b7b3c773309a6e80bf0d44e
ginItchimaru <linoilday@gmail.com>
2023-08-22 17:12:43 +0200

    first commit

---
+++ todo.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+running = True
+while running:
+    usrinput = input("> ")
+
+    if usrinput in ["i", "I"]:
+      print("du hurensohn")
+    
+    elif usrinput == "exit" or usrinput == "Exit":
+      running = False
