1e374417390f52c71459b9e137b160c082c02fe4
ginItchimaru <linoilday@gmail.com>
2023-08-23 13:05:59 +0200

    implemented the write mode

--- todo.py
+++ todo.py
@@ -1,11 +1,27 @@
 #!/usr/bin/env python3
 
+list = open("list.txt", "w")
+
 running = True
 while running:
     usrinput = input("> ")
 
-    if usrinput in ["i", "I"]:
-      print("du hurensohn")
+    if usrinput in ["i", "I", "info"]:
+      print("missing")
     
-    elif usrinput == "exit" or usrinput == "Exit":
+    if usrinput in ["exit", "Exit", "EXIT"]:
       running = False
+    
+    if usrinput in ["W", "w", "write", "Write"]:
+      print("You have entered write mode")
+
+      while True:
+        write_mode_input = input(": ")
+        
+        if write_mode_input in ["exit", "Exit", "EXIT"]:
+          break
+        
+        else:
+          list.write(write_mode_input + "\n")
+          print(f"'{write_mode_input}' has been added")
+
