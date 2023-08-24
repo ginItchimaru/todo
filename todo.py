ea260967cff138110f1268e8f3735515449e2de7
ginItchimaru <linoilday@gmail.com>
2023-08-24 17:00:48 +0200

    working on display mode

--- todo.py
+++ todo.py
@@ -1,16 +1,26 @@
 #!/usr/bin/env python3
 
-list = open("list.txt", "w")
+file_path = "list.txt"
+list = open(file_path, "w")
 
 running = True
 while running:
     usrinput = input("> ")
+    line_count = 0
 
     if usrinput in ["i", "I", "info"]:
       print("missing")
     
     if usrinput in ["exit", "Exit", "EXIT"]:
       running = False
+    
+    if usrinput in ["d", "D", "display", "Display"]:
+      file = open(file_path, "r")
+
+      for line in file:
+        print(f"- {line.rstrip()}")
+        line_count += 1
+        print(line_count)
     
     if usrinput in ["W", "w", "write", "Write"]:
       print("You have entered write mode")
