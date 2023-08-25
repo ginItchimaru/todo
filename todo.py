da287a94be2d565ff11bc213e34c8f5c767d7663
ginItchimaru <linoilday@gmail.com>
2023-08-25 13:45:08 +0200

    fixed display mode

--- todo.py
+++ todo.py
@@ -1,37 +1,41 @@
 #!/usr/bin/env python3
 
 file_path = "list.txt"
-list = open(file_path, "w")
 
 running = True
 while running:
-    usrinput = input("> ")
-    line_count = 0
+  usrinput = input("> ")
+  line_count = 0
 
-    if usrinput in ["i", "I", "info"]:
-      print("missing")
+  if usrinput in ["i", "I", "info"]:
+    print("missing")
     
-    if usrinput in ["exit", "Exit", "EXIT"]:
-      running = False
-    
-    if usrinput in ["d", "D", "display", "Display"]:
-      file = open(file_path, "r")
+  if usrinput in ["exit", "Exit", "EXIT"]:
+    running = False
 
-      for line in file:
-        print(f"- {line.rstrip()}")
-        line_count += 1
-        print(line_count)
+  if usrinput in ["d", "D", "display", "Display"]:
+    try:
+      with open(file_path, "r") as list_read:
+        lines = list_read.readlines()
+        print("Number of lines in the 'lines' list:", len(lines))
     
-    if usrinput in ["W", "w", "write", "Write"]:
-      print("You have entered write mode")
+        for line in lines:
+          print(f"- {line.strip()}")
+          line_count += 1
+    
+    except Exception as e:
+      print("An error occurred:", e)
 
-      while True:
-        write_mode_input = input(": ")
-        
-        if write_mode_input in ["exit", "Exit", "EXIT"]:
-          break
-        
-        else:
-          list.write(write_mode_input + "\n")
+  if usrinput in ["W", "w", "write", "Write"]:
+    print("You have entered write mode")
+
+    while True:
+      write_mode_input = input(": ")
+
+      if write_mode_input in ["exit", "Exit", "EXIT"]:
+        break
+
+      else:
+        with open(file_path, "a") as list_append:
+          list_append.write(write_mode_input + "\n")
           print(f"'{write_mode_input}' has been added")
-
