843706fde4e4dfe4a8ea158d24b9e409143ca022
ginItchimaru <linoilday@gmail.com>
2023-09-05 13:19:56 +0200

    added delete mode

--- todo.py
+++ todo.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 
 file_path = "list.txt"
-line_count = 0
+
 
 def display_list(file_path):
   try:
-      with open(file_path, "r") as list_read:
-        lines = list_read.readlines()
+      with open(file_path, "r") as list:
+        lines = list.readlines()
             
         for line in lines:
           print(f"- {line.strip()}")
@@ -14,18 +14,47 @@ def display_list(file_path):
   except Exception as e:
       print("An error occurred:", e)
   
+
 def write_list(file_path):
 
   while True:
-      write_mode_input = input(": ")
+      write_input = input(": ")
 
-      if write_mode_input in ["exit", "Exit", "EXIT"]:
+      if write_input in ["exit", "Exit", "EXIT"]:
         break
 
       else:
-        with open(file_path, "a") as list_append:
-          list_append.write(write_mode_input + "\n")
-          print(f"'{write_mode_input}' has been added")
+        with open(file_path, "a") as list:
+          list.write(write_input + "\n")
+          print(f"'{write_input}' has been added")
+
+
+def delete_list(file_path):
+
+  while True:
+    delete_input = input(">> ")
+
+    if delete_input in ["exit", "Exit", "EXIT"]:
+      break
+    
+    else:
+      try:
+        line_to_remove = int(delete_input) - 1
+      except ValueError:
+        print("Input must be a valid number.")
+
+      with open(file_path, "r") as list:
+        lines = list.readlines()
+
+    if 0 <= line_to_remove < len(lines):
+      print(f"'{lines[line_to_remove]}' has been removed")
+      del lines[line_to_remove]
+
+      with open(file_path, "w") as list:
+        list.writelines(lines)
+
+    else:
+      print("Line number is out of range.")
 
 
 running = True
@@ -38,9 +67,13 @@ while running:
   if usrinput in ["exit", "Exit", "EXIT"]:
     running = False
 
-  if usrinput in ["d", "D", "display", "Display"]:
+  if usrinput in ["display", "Display", "DISPLAY"]:
     display_list(file_path)
 
-  if usrinput in ["W", "w", "write", "Write"]:
+  if usrinput in ["write", "Write", "WRITE"]:
     print("You have entered write mode")
     write_list(file_path)
+  
+  if usrinput in ["delete", "Delete", "DELETE"]:
+    print("You have entered delete mode")
+    delete_list(file_path)
